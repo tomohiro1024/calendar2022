7ed36ad7a8d0ec2d0e094596cdaf8b47e49369ed
tomohiro <gongyezhihong732@gmail.com>
2023-02-07 10:17:53 +0900

    祝日クラスの作成

---
+++ lib/model/holiday.dart
@@ -0,0 +1,12 @@
+class Holiday {
+  String title;
+  String explanation;
+  DateTime startAt;
+  DateTime endAt;
+
+  Holiday(
+      {required this.title,
+      required this.explanation,
+      required this.startAt,
+      required this.endAt});
+}
