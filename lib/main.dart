831b53e1cb66657ada3eeb93fe78800e8e7e0d48
tomohiro1024 <gongyezhihong732@gmail.com>
2023-02-17 14:03:48 +0900

    Amplify()の設定一旦完了。

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
+import 'package:amplify_api/amplify_api.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:calendar202211/pages/calender.dart';
 import 'package:flutter/material.dart';
 
 import 'amplifyconfiguration.dart';
+import 'models/ModelProvider.dart';
+
+Future<void> _configureAmplify() async {
+  final api = AmplifyAPI(modelProvider: ModelProvider.instance);
+  await Amplify.addPlugin(api);
+
+  try {
+    await Amplify.configure(amplifyconfig);
+  } on AmplifyAlreadyConfiguredException {
+    print('Amplify configure error');
+  }
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Amplify.configure(amplifyconfig);
+  await _configureAmplify();
   runApp(const MyApp());
 }
 
